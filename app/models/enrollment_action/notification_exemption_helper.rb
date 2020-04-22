f239fbe0e2686717771b5486bc4a8aae9b1bf0f6
Trey Evans <tevans@justplainawful.lan>
2020-04-22 15:31:53 -0400

    Add documentation and clear up issue.

--- app/models/enrollment_action/notification_exemption_helper.rb
+++ app/models/enrollment_action/notification_exemption_helper.rb
@@ -1,5 +1,13 @@
 module EnrollmentAction
   module NotificationExemptionHelper
+
+    # Determine if notification should be sent for a termination.
+    #
+    # @param policy [Policy] the policy in question, after termination
+    # @param termination_date [Date] the scheduled termination date
+    # @param check_npt_change [Boolean] check if the NPT flag has changed
+    # @param old_npt_val [Boolean] the value of the NPT flag prior to the change
+    # @return [Boolean]
     def termination_event_exempt_from_notification?(policy, termination_date, check_npt_change = false, old_npt_val = nil)
       return false if policy.is_shop?
       if check_npt_change
