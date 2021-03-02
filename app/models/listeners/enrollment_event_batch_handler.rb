8aa6096ea2775e2aed2c3369080247de5290752d
saimekala07 <saimekala07@gmail.com>
2021-03-02 10:32:05 -0500

    refs # Enchance enrollment batch handling process.

--- app/models/listeners/enrollment_event_batch_handler.rb
+++ app/models/listeners/enrollment_event_batch_handler.rb
@@ -1,42 +1,113 @@
 module Listeners
   class EnrollmentEventBatchHandler < Amqp::Client
+
+    BatchHandler = "info.events.enrollment_batch.handler"
+    BatchCut = "info.events.enrollment_batch.cut"
+    BatchProcess = "info.events.enrollment_batch.process"
+
     def self.queue_name
       ec = ExchangeInformation
       "#{ec.hbx_id}.#{ec.environment}.q.glue.enrollment_event_batch_handler"
     end
 
-    def process
-      events = []
-      responder = ::ExternalEvents::EventResponder.new(
-        "application.gluedb.enrollment_event_batch_handler",
-        channel
-      )
-      di, props, payload = queue.pop({:ack => true, :manual_ack => true})
-      while (di != nil) do
-        headers = props.headers || {}
-        event_message = ExternalEvents::EnrollmentEventNotification.new(
-          responder,
-          di.delivery_tag,
-          extract_timestamp(props),
-          payload,
-          headers
-        )
-        events << event_message
-        di, props, payload = queue.pop({:ack => true, :manual_ack => true})
-      end
-      results = EnrollmentEventProcessingClient.new.call(events)
+    def resource_event_broadcast(level, event_key, r_code, body = "", other_headers = {})
+      event_body = (body.respond_to?(:to_s) ? body.to_s : body.inspect)
+      broadcast_event({
+        :routing_key => "#{level}.application.glue.enrollment_event_handler.#{event_key}",
+        :headers => other_headers.merge({
+          :return_status => r_code.to_s,
+          :submitted_timestamp => Time.now
+        })
+      }, event_body)
     end
 
-   def self.create_queues(chan)
+    def resource_error_broadcast(event_key, r_code, body = "", other_headers = {})
+      resource_event_broadcast("error", event_key, r_code, body, other_headers)
+    end
+
+    def process_enrollment_batch(delivery_info, parsed_event, body, event_time)
+      EnrollmentEvents::Batch.create_batch_and_yield(parsed_event) do |new_batch|
+        resource_event_broadcast("info", "batch_created", "200", body, {
+                                           :employer_hbx_id => new_batch.employer_hbx_id,
+                                           :subscriber_hbx_id => new_batch.subscriber_hbx_id,
+                                           :benefit_kind => new_batch.benefit_kind,
+                                           # :batch_id => new_batch.id,
+                                           :event_time => event_time.to_i.to_s
+                                       })
+      end
+      channel.ack(delivery_info.delivery_tag, false)
+    end
+
+    def process_batch_transaction(delivery_info, parsed_event, body, m_headers, event_time)
+      EnrollmentEvents::Batch.create_batch_transactions_and_yield(parsed_event, body, m_headers, event_time) do |transaction|
+        resource_event_broadcast("info", "batch_transactions_updated", "200", transaction.payload, {
+                                           :employer_hbx_id => transaction.batch.employer_hbx_id,
+                                           :subscriber_hbx_id => transaction.batch.subscriber_hbx_id,
+                                           :benefit_kind => transaction.batch.benefit_kind,
+                                           # :batch_id => transaction.batch_id,
+                                           :event_time => event_time.to_i.to_s
+                                       })
+      end
+      channel.ack(delivery_info.delivery_tag, false)
+    end
+
+    def cut_enrollment_batch
+      if EnrollmentEvents::Batch.where(aasm_state: 'open').any?
+        EnrollmentEvents::Batch.where(aasm_state: 'open').each do |batch|
+           if batch.may_process?
+             batch.process!
+             Amqp::ConfirmedPublisher.with_confirmed_channel(connection) do |chan|
+               ex = chan.fanout(ExchangeInformation.event_publish_exchange, {:durable => true})
+               ex.publish(
+                 "",
+                 { routing_key: BatchProcess,
+                   headers: { batch_id: batch.id }
+                 }
+               )
+             end
+           end
+        end
+      end
+    end
+
+    def on_message(delivery_info, properties, body)
+      m_headers = properties.headers || {}
+      routing_key = delivery_info.routing_key
+      event_time = extract_timestamp(properties)
+      parsed_event = ExternalEvents::EnrollmentEventNotification.new(nil, delivery_info, event_time, body, m_headers)
+      if routing_key == BatchCut
+        cut_enrollment_batch
+        resource_event_broadcast("info", "batch_cut", "200")
+        channel.ack(delivery_info.delivery_tag, false)
+      else
+        begin
+          unless EnrollmentEvents::Batch.new_batch?(parsed_event)
+            process_enrollment_batch(delivery_info,parsed_event, body, event_time)
+            process_batch_transaction(delivery_info, parsed_event, body, m_headers, event_time)
+          else
+            process_batch_transaction(delivery_info, parsed_event, body, m_headers, event_time)
+          end
+        rescue => e
+          resource_error_broadcast("unknown_error", "500", body, m_headers)
+          channel.ack(delivery_info.delivery_tag, false)
+        end
+      end
+    end
+
+    def self.create_queues(chan)
+      ec = ExchangeInformation
+      event_topic_exchange_name = "#{ec.hbx_id}.#{ec.environment}.e.topic.events"
+      event_ex = chan.topic(event_topic_exchange_name, { :durable => true })
       q = chan.queue(
         self.queue_name,
         {
           :durable => true,
           :arguments => {
-            "x-dead-letter-exchange" => (self.queue_name + "-retry")
+                "x-dead-letter-exchange" => (self.queue_name + "-retry")
           }
         }
       )
+      q.bind(event_ex, {:routing_key => "info.events.enrollment_batch.cut"})
       retry_q = chan.queue(
         (self.queue_name + "-retry"),
         {
@@ -48,37 +119,22 @@ module Listeners
         }
       )
       retry_exchange = chan.fanout(
-        (self.queue_name + "-retry")
+          (self.queue_name + "-retry")
       )
       requeue_exchange = chan.fanout(
-        (self.queue_name + "-requeue")
+          (self.queue_name + "-requeue")
       )
       retry_q.bind(retry_exchange, {:routing_key => ""})
       q.bind(requeue_exchange, {:routing_key => ""})
       q
     end
 
-    def self.create_bindings(chan, q)
-      ec = ExchangeInformation
-      event_topic_exchange_name = "#{ec.hbx_id}.#{ec.environment}.e.topic.events"
-      event_topic_exchange = chan.topic(event_topic_exchange_name, {:durable => true})
-      # Replace with correct bindings for enrollment events
-      # q.bind(event_topic_exchange, {:routing_key => "info.events.employer.#"})
-    end
-
-    def self.configure!
-      conn = AmqpConnectionProvider.start_connection
-      chan = conn.create_channel
-      q = create_queues(chan)
-      create_bindings(chan, q)
-      conn.close
-    end
-
     def self.run
       conn = AmqpConnectionProvider.start_connection
       chan = conn.create_channel
       q = create_queues(chan)
-      self.new(chan, q).process
+      chan.prefetch(1)
+      self.new(chan, q).subscribe(:block => true, :manual_ack => true, :ack => true)
       conn.close
     end
   end
