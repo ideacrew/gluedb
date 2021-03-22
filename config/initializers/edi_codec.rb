9d3217f5f58e4f6a5af2ce27c0f502ac01b78bae
Trey Evans <lewis.r.evans@gmail.com>
2021-03-22 11:33:11 -0400

    Update to use new version.

--- config/initializers/edi_codec.rb
+++ config/initializers/edi_codec.rb
@@ -1,2 +1,3 @@
-EdiCodec::Configuration.source_exchange = "#{Settings.site.source_exchange_code}"
+EdiCodec::Configuration.source_exchange = Settings.site.source_exchange_code
+EdiCodec::Configuration.exchange_fein = ExchangeInformation.receiver_id
 
