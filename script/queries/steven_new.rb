2fb28e2804bf2be633695c890449018bd8a69e40
Nikhil S <nikhilks219@gmail.com>
2021-03-25 14:29:00 -0400

    93362: moved benefit status column to w position

--- script/queries/steven_new.rb
+++ script/queries/steven_new.rb
@@ -27,11 +27,11 @@ timestamp = Time.now.strftime('%Y%m%d%H%M')
 Caches::MongoidCache.with_cache_for(Carrier, Plan, Employer) do
 
   CSV.open("stephen_expected_effectuated_20140930_#{timestamp}.csv", 'w') do |csv|
-    csv << ["Subscriber ID", "Member ID" , "Policy ID", "Enrollment Group ID", "Status", "Benefit Status",
+    csv << ["Subscriber ID", "Member ID" , "Policy ID", "Enrollment Group ID", "Status",
             "First Name", "Last Name","SSN", "DOB", "Gender", "Relationship",
             "Plan Name", "HIOS ID", "Plan Metal Level", "Carrier Name",
             "Premium Amount", "Premium Total", "Policy APTC", "Policy Employer Contribution",
-            "Coverage Start", "Coverage End",
+            "Coverage Start", "Coverage End", "Benefit Status",
             "Employer Name", "Employer DBA", "Employer FEIN", "Employer HBX ID",
             "Home Address", "Mailing Address","Email","Phone Number","Broker"]
     policies.each do |pol|
@@ -71,7 +71,7 @@ Caches::MongoidCache.with_cache_for(Carrier, Plan, Employer) do
                 per = en.person
                 next if per.blank?
                 csv << [
-                  subscriber_id, en.m_id, pol._id, pol.eg_id, pol.aasm_state, en.ben_stat,
+                  subscriber_id, en.m_id, pol._id, pol.eg_id, pol.aasm_state,
                   per.name_first,
                   per.name_last,
                   en.member.ssn,
@@ -82,6 +82,7 @@ Caches::MongoidCache.with_cache_for(Carrier, Plan, Employer) do
                   en.pre_amt, pol.pre_amt_tot,pol.applied_aptc, pol.tot_emp_res_amt,
                   en.coverage_start.blank? ? nil : en.coverage_start.strftime("%Y%m%d"),
                   en.coverage_end.blank? ? nil : en.coverage_end.strftime("%Y%m%d"),
+                  en.ben_stat == "cobra" ? en.ben_stat : nil,
                   pol.employer_id.blank? ? nil : employer.name,
                   pol.employer_id.blank? ? nil : employer.dba,
                   pol.employer_id.blank? ? nil : employer.fein,
