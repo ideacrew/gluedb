eb4905c3a14b5cb94ca0f46364d77ca25c6d8027
Matt Williams <m.a.dubya@gmail.com>
2021-04-14 11:56:33 -0400

    try to fix edi_codec initialization error during asset precompile

--- .docker/build.sh
+++ .docker/build.sh
@@ -9,28 +9,32 @@ cp .docker/config/Gemfile .
 cp .docker/config/exchange_information.rb app/models/
 cp .docker/config/production.rb config/environments/
 cp .docker/config/mongoid.yml config/
-cp .docker/unicorn.rb config/
-cp .docker/secrets.yml config/
+cp .docker/config/unicorn.rb config/
+cp .docker/config/secrets.yml config/
 
 cp -r ../designmodo-flatuipro-rails .
 cp -r ../edi_codec .
+
+ls -ltr 
 
 docker build --build-arg BUNDLER_VERSION_OVERRIDE='1.17.3' \
              --build-arg EDIDB_DB_HOST='host.docker.internal' \
              --build-arg EDIDB_DB_PORT="27017" \
              --build-arg EDIDB_DB_NAME="edidb_prod" \
+             --build-arg EDIDB_FQDN="localhost" \
+	     --build-arg RECEIVER_ID="000000001" \
              --build-arg RABBITMQ_URL="amqp://guest:guest@host.docker.internal:5672" \
 	     --build-arg EDIDB_DEVISE_SECRET_KEY="4949641a374994854c0529feb329a81885867f044eb6c23102892e38bb32da437a94ee27eb4086b196f7273868d4b06c682948f5ced62385c548ba2d96898e20" \
              --build-arg EDIDB_SECRET_KEY_BASE="c8d2b9b204fbac78081a88a2c29b28cfeb82e6ccd3664b3948b813463b5917b315dbbd3040e8dffcb5b68df427099db0ce03e59e2432dfe5d272923b00755b82" \
              -f .docker/production/Dockerfile --target app -t $2:$1 .
 docker push $2:$1
 
-cp Gemfile.tmp Gemfile
-cp app/models/exchange_information.rb.tmp app/models/exchange_information.rb
-cp config/environments/production.rb.tmp config/environments/production.rb
-cp config/initializers/devise.rb.tmp config/initializers/devise.rb
-cp config/mongoid.yml.tmp config/mongoid.yml
-cp config/unicorn.rb.tmp config/unicorn.rb
+mv Gemfile.tmp Gemfile
+mv app/models/exchange_information.rb.tmp app/models/exchange_information.rb
+mv config/environments/production.rb.tmp config/environments/production.rb
+mv config/initializers/devise.rb.tmp config/initializers/devise.rb
+mv config/mongoid.yml.tmp config/mongoid.yml
+mv config/unicorn.rb.tmp config/unicorn.rb
 rm config/secrets.yml
 rm -rf designmodo-flatuipro-rails 
 rm -rf edi_codec
