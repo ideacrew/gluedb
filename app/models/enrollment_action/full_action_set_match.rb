ec4a67dc725684e1eb8d927d75babf3a99c093a8
Trey Evans <lewis.r.evans@gmail.com>
2021-06-17 14:13:56 -0400

    Solution example for special cases.

---
+++ app/models/enrollment_action/full_action_set_match.rb
@@ -0,0 +1,21 @@
+module EnrollmentAction
+  # See if the entire enrollment action set can be made to match a single
+  # scenario.
+  #
+  # This is very rare and occurs currently only in the following cases:
+  # * "buy last year and cancel my renewal"
+  # * "add a dependent last year and cancel my renewal"
+  class FullActionSetMatch
+    def self.call(enrollments)
+      check_match(enrollments)
+    end
+
+    def self.check_match(enrollments)
+      return [false, []] unless enrollments.length == 2
+      # Here we are going to insert the special case of an exact match with
+      # exactly that number of elements.  If true, we're going to return
+      # a tuple of [true, enrollments]. Otherwise, [false, []].
+      [false, []]
+    end
+  end
+end
