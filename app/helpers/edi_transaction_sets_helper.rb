1ee9ecdbe6b803941a6de26deee86a3a7aed7cbb
Trey Evans <lewis.r.evans@gmail.com>
2021-07-30 10:53:44 -0400

    Fix crash on missing sender.

--- app/helpers/edi_transaction_sets_helper.rb
+++ app/helpers/edi_transaction_sets_helper.rb
@@ -1,2 +1,8 @@
 module EdiTransactionSetsHelper
+
+  def transaction_error_sender_name
+    sender = t.transmission.sender
+    return sender.name if sender
+    "No sender for ISA CODE #{t.transmission.ic_sender_id}"
+  end
 end
