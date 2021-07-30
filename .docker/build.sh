f55d1677b9c303d598f23b9fbf9ebcf8fa12ae8d
Trey Evans <lewis.r.evans@gmail.com>
2021-07-30 13:16:53 -0400

    Make tokens come in via ENV, remove them from file.

--- .docker/build.sh
+++ .docker/build.sh
@@ -26,7 +26,7 @@ docker build --build-arg BUNDLER_VERSION_OVERRIDE='1.17.3' \
              --build-arg RABBITMQ_URL="amqp://guest:guest@host.docker.internal:5672" \
 	           --build-arg EDIDB_DEVISE_SECRET_KEY="4949641a374994854c0529feb329a81885867f044eb6c23102892e38bb32da437a94ee27eb4086b196f7273868d4b06c682948f5ced62385c548ba2d96898e20" \
              --build-arg EDIDB_SECRET_KEY_BASE="c8d2b9b204fbac78081a88a2c29b28cfeb82e6ccd3664b3948b813463b5917b315dbbd3040e8dffcb5b68df427099db0ce03e59e2432dfe5d272923b00755b82" \
-             --build-arg GEM_OAUTH_TOKEN="INSERT TOKEN HERE"
+             --build-arg GEM_OAUTH_TOKEN=$GEM_OAUTH_TOKEN
              -f .docker/production/Dockerfile --target app -t $2:$1 .
 docker push $2:$1
 
