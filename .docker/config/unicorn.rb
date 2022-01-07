399e36f09c7229ef161e529a05738a224b100528
saipraveen18 <saipraveen.gudimetla@gmail.com>
2022-01-07 11:29:20 -0500

    reduce number of unicorn worker processors

--- .docker/config/unicorn.rb
+++ .docker/config/unicorn.rb
@@ -5,5 +5,5 @@ stdout_path "/edidb/log/unicorn.log"
 
 #listen "/edidb/tmp/unicorn.ap.sock"
 listen 3000
-worker_processes 10
+worker_processes 2
 timeout 180
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,5 +4,5 @@ stderr_path "/var/www/deployments/gluedb/shared/log/unicorn.log"
 stdout_path "/var/www/deployments/gluedb/shared/log/unicorn.log"
 
 listen "/tmp/unicorn.ap.sock"
-worker_processes 10
+worker_processes 2
 timeout 180
