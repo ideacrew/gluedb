81855c8fca10bec754e4f5e3dffff90b50c3a830
Sai Mekala <saimekala07@gmail.com>
2022-03-16 10:31:55 -0400

    Fix Term Date on Dep Drop (#87)
    * Fix member term date
    
    * Fix specs

--- spec/models/enrollment_action/dependent_drop_spec.rb
+++ spec/models/enrollment_action/dependent_drop_spec.rb
@@ -47,7 +47,7 @@ describe EnrollmentAction::DependentDrop, "given a qualified enrollment set, bei
   let(:terminated_policy_cv) { instance_double(Openhbx::Cv2::Policy, :enrollees => [member_primary, member_secondary, member_drop])}
   let(:new_policy_cv) { instance_double(Openhbx::Cv2::Policy, :enrollees => [member_primary, member_secondary]) }
   let(:plan) { instance_double(Plan, :id => 1) }
-  let(:policy) { instance_double(Policy, :hbx_enrollment_ids => [1,2,3], carrier: carrier) }
+  let(:policy) { instance_double(Policy, :hbx_enrollment_ids => [1,2,3], carrier: carrier, policy_start: Date.new(2022)) }
   let(:active_policy) { instance_double(Policy) }
 
   let(:dependent_drop_event) { instance_double(
@@ -56,12 +56,14 @@ describe EnrollmentAction::DependentDrop, "given a qualified enrollment set, bei
     :existing_plan => plan,
     :all_member_ids => [1,2],
     :hbx_enrollment_id => 1,
-    :subscriber_start => Date.new(2022)
+    :subscriber_start => Date.new(2022),
+    :subscriber_end => Date.new(2022)
     ) }
   let(:termination_event) { instance_double(
     ::ExternalEvents::EnrollmentEventNotification,
     :policy_cv => terminated_policy_cv,
     :existing_policy => policy,
+    :subscriber_end => Date.new(2022),
     :all_member_ids => [1,2,3]
     ) }
 
@@ -78,6 +80,7 @@ describe EnrollmentAction::DependentDrop, "given a qualified enrollment set, bei
     allow(policy_updater).to receive(:use_totals_from).with(new_policy_cv)
     allow(policy_updater).to receive(:persist).and_return(true)
     allow(policy_updater).to receive(:subscriber_start).with(Date.new(2022)).and_return(true)
+    allow(policy_updater).to receive(:member_drop_date).with(Date.new(2022)).and_return(true)
     allow(subject).to receive(:same_carrier_renewal_candidates).with(dependent_drop_event).and_return([active_policy])
     allow(dependent_drop_event).to receive(:dep_add_or_drop_to_renewal_policy?).with(active_policy, policy).and_return(false)
   end
@@ -98,11 +101,11 @@ describe EnrollmentAction::DependentDrop, "given a qualified enrollment set, bei
   let(:termination_event_xml) { double }
   let(:event_xml) { double }
   let(:event_responder) { instance_double(::ExternalEvents::EventResponder, :connection => amqp_connection) }
-  let(:enrollee_primary) { double(:m_id => 1, :coverage_start => :one_month_ago, :c_id => nil, :cp_id => nil) }
-  let(:enrollee_new) { double(:m_id => 2, :coverage_start => :one_month_ago, :c_id => nil, :cp_id => nil) }
+  let(:enrollee_primary) { double(:m_id => 1, :coverage_start => :one_month_ago, coverage_end: :one_month_ago, :c_id => nil, :cp_id => nil) }
+  let(:enrollee_new) { double(:m_id => 2, :coverage_start => :one_month_ago, :coverage_end => :one_month_ago, :c_id => nil, :cp_id => nil) }
   let(:carrier) { instance_double(Carrier, :renewal_dependent_drop_transmitted_as_renewal => true) }
   let(:plan) { instance_double(Plan, :id => 1, ) }
-  let(:policy) { instance_double(Policy, :enrollees => [enrollee_primary, enrollee_new], :eg_id => 1, carrier: carrier) }
+  let(:policy) { instance_double(Policy, :enrollees => [enrollee_primary, enrollee_new], :eg_id => 1, carrier: carrier, policy_start: Date.new(2022)) }
   let(:active_policy) { instance_double(Policy) }
 
   let(:dependent_drop_event) { instance_double(
@@ -134,6 +137,7 @@ describe EnrollmentAction::DependentDrop, "given a qualified enrollment set, bei
     allow(action_publish_helper).to receive(:set_event_action).with("urn:openhbx:terms:v1:enrollment#change_member_terminate")
     allow(action_publish_helper).to receive(:set_policy_id).with(1).and_return(true)
     allow(action_publish_helper).to receive(:set_member_starts).with({ 1 => :one_month_ago, 2 => :one_month_ago })
+    allow(action_publish_helper).to receive(:set_member_end_date).with({ 1 => :one_month_ago, 2 => :one_month_ago })
     allow(action_publish_helper).to receive(:filter_affected_members).with([3]).and_return(true)
     allow(action_publish_helper).to receive(:replace_premium_totals).with(event_xml)
     allow(action_publish_helper).to receive(:keep_member_ends).with([3])
@@ -154,6 +158,11 @@ describe EnrollmentAction::DependentDrop, "given a qualified enrollment set, bei
 
   it "sets member start dates" do
     expect(action_publish_helper).to receive(:set_member_starts).with({ 1 => :one_month_ago, 2 => :one_month_ago })
+    subject.publish
+  end
+
+  it "sets member end dates" do
+    expect(action_publish_helper).to receive(:set_member_end_date).with({ 1 => :one_month_ago, 2 => :one_month_ago })
     subject.publish
   end
 
@@ -424,6 +433,255 @@ describe "given renewal event, dependent drop from IVL renewal policy", :dbclean
       expect(Policy.where(hbx_enrollment_ids: dependent_add_event.hbx_enrollment_id).count).to eq 1
 
       expect_any_instance_of(EnrollmentAction::ActionPublishHelper).to receive(:set_event_action).with("urn:openhbx:terms:v1:enrollment#auto_renew")
+      subject.publish
+    end
+  end
+end
+
+describe "given prospective dependent drop", :dbclean => :after_each do
+  let(:amqp_connection) { double }
+  let(:event_xml) { double }
+  let(:event_responder) { instance_double(::ExternalEvents::EventResponder, :connection => amqp_connection) }
+  let(:eg_id) { '1' }
+  let(:carrier_id) { '2' }
+  let(:kind) { 'individual' }
+
+  let(:new_applied_aptc_amount) { 200.0 }
+  let(:new_premium_total_amount) { 300.0 }
+  let(:new_total_responsible_amount) { 100.0 }
+
+  let(:old_applied_aptc_amount) { 50.0 }
+  let(:old_premium_total_amount) { 100.0 }
+  let(:old_total_responsible_amount) { 50.0 }
+
+  let(:carrier) { Carrier.create!(:renewal_dependent_drop_transmitted_as_renewal => true) }
+  let(:active_plan) { Plan.create!(:name => "test_plan", carrier_id: carrier_id, :coverage_type => "health", year: Date.today.year) }
+  let!(:primary) {
+    person = FactoryGirl.create :person
+    person.update(authority_member_id: person.members.first.hbx_member_id)
+    person
+  }
+  let!(:dep) {
+    person = FactoryGirl.create :person
+    person.update(authority_member_id: person.members.first.hbx_member_id)
+    person
+  }
+  let(:dep_coverage_drop_date) { Date.today.beginning_of_year + 2.month }
+  let(:active_enrollee1) { Enrollee.new(m_id: primary.authority_member.hbx_member_id, rel_code: 'self', coverage_start: Date.today.beginning_of_year, coverage_end: '', :c_id => nil, :cp_id => nil)}
+  let(:active_enrollee2) { Enrollee.new(m_id: dep.authority_member.hbx_member_id, rel_code: 'child', coverage_start: Date.today.beginning_of_year, coverage_end: '', :c_id => nil, :cp_id => nil)}
+
+  let!(:active_policy) {
+    policy =  Policy.create(enrollment_group_id: eg_id, hbx_enrollment_ids: ["123"], carrier_id: carrier_id, plan: active_plan, carrier: carrier, coverage_start: Date.today.beginning_of_year, coverage_end: nil, kind: kind)
+    policy.update_attributes(enrollees: [active_enrollee1, active_enrollee2],
+                             pre_amt_tot: old_premium_total_amount,
+                             tot_res_amt: old_total_responsible_amount,
+                             applied_aptc: old_applied_aptc_amount,
+                             hbx_enrollment_ids: ["123"])
+    policy.save
+    policy
+  }
+
+  let(:xml_after_dependent_drop) { <<-EVENTXML
+   <enrollment_event xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://openhbx.org/api/terms/1.0'>
+   <header>
+     <hbx_id>29035</hbx_id>
+     <submitted_timestamp>2016-11-08T17:44:49</submitted_timestamp>
+   </header>
+   <event>
+     <body>
+       <enrollment_event_body xmlns="http://openhbx.org/api/terms/1.0">
+         <affected_members>
+           <affected_member>
+              <member>
+               <id><id>#{primary.authority_member.hbx_member_id}</id></id>
+             </member>
+             <benefit>
+               <premium_amount>465.13</premium_amount>
+               <begin_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</begin_date>
+             </benefit>
+           </affected_member>
+         </affected_members>
+         <enrollment xmlns="http://openhbx.org/api/terms/1.0">
+           <policy>
+             <id>
+               <id>123</id>
+             </id>
+           <enrollees>
+             <enrollee>
+               <member>
+                 <id><id>#{primary.authority_member.hbx_member_id}</id></id>
+               </member>
+               <is_subscriber>true</is_subscriber>
+               <benefit>
+                 <premium_amount>111.11</premium_amount>
+                 <begin_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</begin_date>
+               </benefit>
+             </enrollee>
+           </enrollees>
+           <enrollment>
+            <plan>
+               <id>
+                  <id>#{active_plan.hios_plan_id}</id>
+               </id>
+               <name>BluePreferred PPO Standard Platinum $0</name>
+               <active_year>#{active_plan.year}</active_year>
+               <is_dental_only>false</is_dental_only>
+               <carrier>
+                 <id>
+                   <id>#{carrier.hbx_carrier_id}</id>
+                 </id>
+                 <name>CareFirst</name>
+               </carrier>
+               <metal_level>urn:openhbx:terms:v1:plan_metal_level#platinum</metal_level>
+               <coverage_type>urn:openhbx:terms:v1:qhp_benefit_coverage#health</coverage_type>
+               <ehb_percent>99.64</ehb_percent>
+             </plan>
+           <individual_market>
+             <assistance_effective_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</assistance_effective_date>
+             <applied_aptc_amount>#{new_applied_aptc_amount}</applied_aptc_amount>
+           </individual_market>
+           <premium_total_amount>#{new_premium_total_amount}</premium_total_amount>
+           <total_responsible_amount>#{new_total_responsible_amount}</total_responsible_amount>
+           </enrollment>
+           </policy>
+         </enrollment>
+         </enrollment_event_body>
+     </body>
+   </event>
+ </enrollment_event>
+  EVENTXML
+  }
+  let(:termination_xml) { <<-EVENTXML
+   <enrollment_event xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://openhbx.org/api/terms/1.0'>
+   <header>
+     <hbx_id>29035</hbx_id>
+     <submitted_timestamp>2016-11-08T17:44:49</submitted_timestamp>
+   </header>
+   <event>
+     <body>
+       <enrollment_event_body xmlns="http://openhbx.org/api/terms/1.0">
+         <affected_members>
+           <affected_member>
+             <member>
+               <id><id>#{primary.authority_member.hbx_member_id}</id></id>
+             </member>
+             <benefit>
+               <premium_amount>465.13</premium_amount>
+               <begin_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</begin_date>
+               <end_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</end_date>
+             </benefit>
+           </affected_member>
+           <affected_member>
+             <member>
+               <id><id>#{dep.authority_member.hbx_member_id}</id></id>
+             </member>
+             <benefit>
+               <premium_amount>465.13</premium_amount>
+               <begin_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</begin_date>
+               <end_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</end_date>
+             </benefit>
+           </affected_member>
+         </affected_members>
+         <enrollment xmlns="http://openhbx.org/api/terms/1.0">
+           <policy>
+             <id>
+               <id>123</id>
+             </id>
+           <enrollees>
+             <enrollee>
+               <member>
+                 <id><id>#{primary.authority_member.hbx_member_id}</id></id>
+               </member>
+               <is_subscriber>true</is_subscriber>
+               <benefit>
+                 <premium_amount>111.11</premium_amount>
+                 <begin_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</begin_date>
+                 <end_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</end_date>
+               </benefit>
+             </enrollee>
+             <enrollee>
+               <member>
+                 <id><id>#{dep.authority_member.hbx_member_id}</id></id>
+               </member>
+               <is_subscriber>false</is_subscriber>
+               <benefit>
+                 <premium_amount>111.11</premium_amount>
+                  <begin_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</begin_date>
+                  <end_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</end_date>
+               </benefit>
+             </enrollee>
+           </enrollees>
+           <enrollment>
+            <plan>
+               <id>
+                  <id>#{active_plan.hios_plan_id}</id>
+               </id>
+               <name>BluePreferred PPO Standard Platinum $0</name>
+               <active_year>#{active_plan.year}</active_year>
+               <is_dental_only>false</is_dental_only>
+               <carrier>
+                 <id>
+                   <id>#{carrier.hbx_carrier_id}</id>
+                 </id>
+                 <name>CareFirst</name>
+               </carrier>
+               <metal_level>urn:openhbx:terms:v1:plan_metal_level#platinum</metal_level>
+               <coverage_type>urn:openhbx:terms:v1:qhp_benefit_coverage#health</coverage_type>
+               <ehb_percent>99.64</ehb_percent>
+             </plan>
+           <individual_market>
+             <assistance_effective_date>#{dep_coverage_drop_date.strftime("%Y%m%d")}</assistance_effective_date>
+             <applied_aptc_amount>#{old_applied_aptc_amount}</applied_aptc_amount>
+           </individual_market>
+           <premium_total_amount>#{old_premium_total_amount}</premium_total_amount>
+           <total_responsible_amount>#{old_total_responsible_amount}</total_responsible_amount>
+           </enrollment>
+           </policy>
+         </enrollment>
+         </enrollment_event_body>
+     </body>
+   </event>
+ </enrollment_event>
+  EVENTXML
+  }
+  let(:m_tag) { double('m_tag') }
+  let(:t_stamp) { double('t_stamp') }
+  let(:headers) { double('headers') }
+  let(:event_responder) { instance_double(::ExternalEvents::EventResponder, :connection => amqp_connection) }
+  let :termination_event do
+    ::ExternalEvents::EnrollmentEventNotification.new event_responder, m_tag, t_stamp, termination_xml, headers
+  end
+  let :event_after_dependent_drop do
+    ::ExternalEvents::EnrollmentEventNotification.new event_responder, m_tag, t_stamp, xml_after_dependent_drop, headers
+  end
+  subject do
+    EnrollmentAction::DependentDrop.new(termination_event, event_after_dependent_drop)
+  end
+
+  let(:connection) { double }
+  let(:event_broadcaster) do
+    instance_double(Amqp::EventBroadcaster)
+  end
+
+  context "#qualifies #persist #publish" do
+    before do
+      allow(Amqp::EventBroadcaster).to receive(:with_broadcaster).and_yield(event_broadcaster)
+      allow(event_broadcaster).to receive(:broadcast)
+    end
+
+    it "should terminate dependent with correct end date" do
+      expect(EnrollmentAction::DependentDrop.qualifies?([termination_event, event_after_dependent_drop])).to be_truthy
+
+      expect(Policy.all.last.enrollees.where(m_id: active_enrollee2.m_id).last.coverage_end).to eq nil
+
+      expect(subject.persist).to be_truthy
+      active_policy.reload
+      expect(Policy.where(hbx_enrollment_ids: event_after_dependent_drop.hbx_enrollment_id).count).to eq 1
+
+      # Should terminate enrollee with correct end date
+      expect(Policy.all.last.enrollees.where(m_id: active_enrollee2.m_id).last.coverage_end).to eq dep_coverage_drop_date - 1.day
+
+      expect_any_instance_of(EnrollmentAction::ActionPublishHelper).to receive(:set_event_action).with("urn:openhbx:terms:v1:enrollment#change_member_terminate")
       subject.publish
     end
   end
