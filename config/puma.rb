66b8bac38b0613965dae6f47b7c1264f5851810c
Matt Williams <m.a.dubya@gmail.com>
2022-05-11 17:21:45 -0400

    bump threads

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,7 @@
 environment ENV.fetch("RAILS_ENV", "development")
 bind "tcp://0.0.0.0:#{ENV['PORT'] || 3000}"
 pidfile "/edidb/tmp/pids/puma.pid"
-threads 1,1
+#threads 1,1
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
