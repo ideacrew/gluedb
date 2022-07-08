cc51cb695fdd904fba6963fa0cc0e0f8b278abb3
nks2109 <nikhilks219@gmail.com>
2022-07-08 14:16:31 -0400

    pbp generation shell script

---
+++ lib/pbp_generation.rb
@@ -0,0 +1,2 @@
+pbp = Generators::Reports::SbmiSerializer.new({calendar_year: 2022})
+pbp.process
---
+++ pbp_gen.sh
@@ -0,0 +1,2 @@
+bundle exec rails r lib/pbp_generation.rb -e production
+echo "generation completed"$(date)
