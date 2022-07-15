de8a596d8c23606482c0d2aad34b10bfab86c6f8
Sai Mekala <saimekala07@gmail.com>
2022-07-15 12:04:27 -0400

    update aptc credits when importing EDI for carrier terms (#113)

--- app/models/parsers/edi/incoming_transaction.rb
+++ app/models/parsers/edi/incoming_transaction.rb
@@ -149,6 +149,7 @@ module Parsers
             enrollee.termed_by_carrier = true
           end
         end
+        @policy.update_aptc_credit(@policy.policy_end)
         @policy.save
       end
 
