7cf9fab26ca127715f6d4ccfc2443e4e30d8bae3
Nikhil S <nikhilks219@gmail.com>
2022-09-15 11:13:49 -0400

    99304: Displays Calculated OSSE Contribution for OSSE Policies in GlueDB (#125)

--- app/helpers/policies_helper.rb
+++ app/helpers/policies_helper.rb
@@ -11,4 +11,21 @@ module PoliciesHelper
   def disable_radio_button?(policy)
     ["canceled", "carrier_canceled"].include? policy.aasm_state
   end
+
+  def as_dollars(val)
+    BigDecimal.new(val.to_s).round(2)
+  end
+
+  #osse calculation
+  def osse_amt(policy)
+    total_premium_amount = policy.pre_amt_tot.to_f
+    total_responsible_amount = policy.tot_res_amt.to_f
+    employer_contribution = policy.tot_emp_res_amt.to_f
+    aptc_amount = policy.applied_aptc.to_f
+    if policy.employer_id.present?
+      as_dollars(total_premium_amount - employer_contribution - total_responsible_amount).to_f
+    else
+      as_dollars(total_premium_amount - aptc_amount - total_responsible_amount).to_f
+    end
+  end
 end
