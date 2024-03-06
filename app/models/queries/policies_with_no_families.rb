94f455f90ed007140769b50779fe466b25ec06cd
Nikhil S <nikhilks219@gmail.com>
2024-03-06 15:51:52 -0500

    updating family to policy association script to handle aggregation query (H36 EOM) (#179)

--- app/models/queries/policies_with_no_families.rb
+++ app/models/queries/policies_with_no_families.rb
@@ -4,8 +4,11 @@ module Queries
     # returns policies that do not belong to any family
     def execute
       policy_ids = Family.collection.aggregate([
+        {"$skip" => 0},
+        {"$limit" => 100000},
         {"$unwind" => '$households'},
         {"$unwind" => '$households.hbx_enrollments'},
+        {"$match" => {"households.hbx_enrollments.kind" => {"$ne" => "employer_sponsored"}}},
         {
           "$group" => {
             "_id" => {
@@ -22,7 +25,50 @@ module Queries
            }
            ]).collect{|r| r['policy_id']}.uniq
 
-      policies = Policy.where(:id.nin => policy_ids).individual_market
+      next_policy_ids = Family.collection.aggregate([
+        {"$skip" => 100000},
+        {"$limit" => 100000},
+        {"$unwind" => '$households'},
+        {"$unwind" => '$households.hbx_enrollments'},
+        {"$match" => {"households.hbx_enrollments.kind" => {"$ne" => "employer_sponsored"}}},
+        {
+          "$group" => {
+            "_id" => {
+              "id" => "$households.hbx_enrollments._id"
+              },
+              "policy_id" => {"$last" => "$households.hbx_enrollments.policy_id"},
+            }
+            },
+            {
+              "$project" => {
+               "_id" => 1,
+               "policy_id" => "$policy_id",
+             }
+           }
+           ]).collect{|r| r['policy_id']}.uniq
+      more_policy_ids = Family.collection.aggregate([
+        {"$skip" => 200000},
+        {"$unwind" => '$households'},
+        {"$unwind" => '$households.hbx_enrollments'},
+        {"$match" => {"households.hbx_enrollments.kind" => {"$ne" => "employer_sponsored"}}},
+        {
+          "$group" => {
+            "_id" => {
+              "id" => "$households.hbx_enrollments._id"
+              },
+              "policy_id" => {"$last" => "$households.hbx_enrollments.policy_id"},
+            }
+            },
+            {
+              "$project" => {
+               "_id" => 1,
+               "policy_id" => "$policy_id",
+             }
+           }
+           ]).collect{|r| r['policy_id']}.uniq
+      all_policy_ids = next_policy_ids + policy_ids + more_policy_ids
+
+      policies = Policy.where(:id.nin => all_policy_ids).individual_market
     end
   end
 end
